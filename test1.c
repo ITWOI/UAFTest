6cb45c27e810c7d6eac450a2bfc72a17e8b167b4
WY <fff000147369@gmail.com>
2016-08-10 22:40:49 +0800

    add more test cases

--- test1.c
+++ test1.c
@@ -26,11 +26,40 @@ int test3()
     return 1;
 }
 
+int test4()
+{
+    char* p = (char*)malloc(10);
+    char* q = p;
+    char* p1 = q;
+    free(p);
+    *p1='1';
+    return 1;
+}
+char *gp;
+int test5()
+{
+    char* gp= (char*)malloc(10);
+    char* p = gp;
+    free(p);
+    return 1;
+}
+int test6(char* p)
+{
+    p= (char*)malloc(10);
+    free(p);
+    return 1;
+}
 int main()
 {
     test1();
     test2();
     test3();
+    test4();
+    test5();
 
+    *gp='1';
+    char*mp;
+    test6(mp);
+    *mp='1';
     return 0;
 }
