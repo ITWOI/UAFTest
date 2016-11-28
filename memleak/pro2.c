a5d7210324e26d57b127aa2753b2f148dd72fe75
WY <fff000147369@gmail.com>
2016-11-28 00:13:58 -0800

    modify it to a more complicated one.

--- memleak/pro2.c
+++ memleak/pro2.c
@@ -1,6 +1,7 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include "time.h"
 
 int hash(char *p)
 {
@@ -22,23 +23,29 @@ void memPro(char *p, char **p_1, char *q, char **q_1)
 
 int main()
 {
+    clock_t start, finish;
+    start = clock();
+
     char *p = (char *)malloc(sizeof(char)*10);
     char **p_1 = &p;
     char *q;
     char **q_1 = &p;
     int i = 0;
     strcpy(p,"hello");
-    for(; i<100; i++)
+    for(; i<100000; i++)
     {
         //TODO: I should explain the malloc function at DangDone.
         //We don't need to chage reference for memory leak.
-        q = (char *)malloc(sizeof(char)*10);
+        q = (char *)malloc(sizeof(char)*4096);
         q = p;
         q_1 = p_1;
         strcpy(q,"hello");
+        sprintf(q,"%d", i);
         memPro(p, p_1, q, q_1);
     }
     strcat(p,"world");
 
+    finish = clock();
+    printf("%f seconds\n", (double)(finish - start) / CLOCKS_PER_SEC);
     return 0;
 }
--- memleak/vul2.c
+++ memleak/vul2.c
@@ -1,21 +1,27 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-
+#include "time.h"
 
 int main()
 {
+    clock_t start, finish;
+    start = clock();
+
     char *p = (char *)malloc(sizeof(char)*10);
     char *q;
     int i = 0;
     strcpy(p,"hello");
-    for(; i<100; i++)
+    for(; i<100000; i++)
     {
-        q = (char *)malloc(sizeof(char)*10);
+        q = (char *)malloc(sizeof(char)*4096);
         q = p;
         strcpy(q,"hello");
+        sprintf(q,"%d", i);
     }
     strcat(p,"world");
 
+    finish = clock();
+    printf("%f seconds\n", (double)(finish - start) / CLOCKS_PER_SEC);
     return 0;
 }
